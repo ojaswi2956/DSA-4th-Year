60c92eb2a98a325fcd9a0067da276678ffdaa994
Ojaswi <ojaswiahuja12@gmail.com>
2026-07-17 09:27:13 +0530

    Added Closet Sum solution

---
+++ Day7/ClosetSum.java
@@ -0,0 +1,40 @@
+package Day7;
+import java.util.*;
+
+public class ClosetSum{
+    public static int SumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+                if (currentSum == target) {
+                    return currentSum;
+                }
+                if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
+                    closestSum = currentSum;
+                }
+                if (currentSum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return closestSum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int result = SumClosest(nums, target);
+        System.out.println(result);
+        sc.close();
+    }
+}
